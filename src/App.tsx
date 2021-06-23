9e1f84f23bf029a52527829b08a8c76152b0a1b8
juvenalbruno <bruno.andrade178@hotmail.com>
2021-06-23 01:08:14 -0300

    Aula_2

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
-import './App.css';
+import { Route, BrowserRouter } from 'react-router-dom';
+
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+
+import { AuthContextProvaider } from './contexts/AuthContext';
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <BrowserRouter>
+      <AuthContextProvaider>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+      </AuthContextProvaider>
+    </BrowserRouter>
   );
 }
 
